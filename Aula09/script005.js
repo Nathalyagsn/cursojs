945c1db00979c48d53cf23cd0bcbe7b8ba5633d7
Nathalya <nathalya.gsn@gmail.com>
2025-01-30 10:55:37 -0300

    DOM

---
+++ Aula09/script005.js
@@ -0,0 +1,7 @@
+var corpo = document.body
+var p1 = document.getElementsByTagName('p')[1]
+var d  = document.getElementById('msg')
+
+//d.style.background = 'green'
+//d.innerText = 'Estou aguardando...'
+
