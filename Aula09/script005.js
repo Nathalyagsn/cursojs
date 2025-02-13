a051a9e462fd88c1fcd07f1141d8b844e9a90cc4
Nathalya Silva <nathalya.gsn@gmail.com>
2025-02-13 11:26:08 -0300

    aula 10

--- Aula09/script005.js
+++ Aula09/script005.js
@@ -2,6 +2,7 @@ var corpo = document.body
 var p1 = document.getElementsByTagName('p')[1]
 var d  = document.getElementById('msg')
 
+
 //d.style.background = 'green'
-//d.innerText = 'Estou aguardando...'
+d.innerText = 'Estou aguardando...'
 
---
+++ Aula10/script.js
@@ -0,0 +1,15 @@
+function clicar() {
+    var area = window.document.getElementById('area')
+    area.innerText = 'Clicado'
+    
+}
+
+function entrar() {
+    var area = window.document.getElementById('area')
+    area.innerText = 'Clique'
+}
+
+function sair(){
+    var area = window.document.getElementById('area')
+    area.innerText = 'Interaja'
+}
