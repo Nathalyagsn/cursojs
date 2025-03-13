cd74eaf4ce2dbec6ed31ceddda4fbb34cf03ce12
Nathalya Silva <nathalya.gsn@gmail.com>
2025-03-13 11:50:26 -0300

    aula16

---
+++ Aula16/funcao1.js
@@ -0,0 +1,15 @@
+function parImpar(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+// let res = parImpar(3)
+
+// console.log(res)
+
+console.log(parImpar(2))
+
+
