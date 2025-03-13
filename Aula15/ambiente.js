38f31a500f8597375725c8f11ac77a5e958b661b
Nathalya Silva <nathalya.gsn@gmail.com>
2025-03-13 10:46:25 -0300

    aula 15

---
+++ Aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(7)
+
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(num.length)
+console.log(num.sort())
---
+++ Aula15/burcandoValores.js
@@ -0,0 +1,4 @@
+valores = [8,1,7,4,2,9]
+
+console.log(`O número se econtra na posição ${valores.indexOf(7)}`)
+
